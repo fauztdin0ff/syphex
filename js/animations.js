8faa74f18eb3ffb73de04c42cf0c1b374aaa2bec
Fauztdinov Damir <112619096+fauztdin0ff@users.noreply.github.com>
2025-04-07 22:46:44 +0500

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
    gsap.registerPlugin(ScrollTrigger);
 
-   ScrollTrigger.normalizeScroll(true);
    
    // Проверка существования элементов
    function elementExists(selector) {
